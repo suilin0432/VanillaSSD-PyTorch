578d71570a876d2e431fe53fbd1938307d139659
隋霖 <401569798@qq.com>
2019-04-02 09:34:58 +0800

    update

---
+++ SSDModel.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layers import *
+from data import voc, coco
+import os
---
+++ data/__init__.py
@@ -0,0 +1,2 @@
+# 从配置文件中引入 SSD的相关设置
+from .config import *
